f0edfd3e8808e8e9e1f7e29550246202a3e15dce
Aananda <atimalsina2016@gmail.com>
2025-07-30 13:05:56 +0545

    learnrandom and rockpaperscissors

---
+++ learn_random.py
@@ -0,0 +1,39 @@
+import random
+
+#random_integer = random.randint(1,10)
+#print(random_integer)
+
+# random_number =random.random()*10
+# print(random_number)
+
+# random_float = random.uniform(1,10)
+# print(random_float)
+
+# rand_num = random.random()
+# print(rand_num)
+# if(rand_num<=0.5):
+#     print("Heads")
+
+# else:
+#     print("Tails")
+
+# num = [1,2,3]
+# num.append(2)
+# print(num)
+
+# num = [1,2,3]
+# #num.extend([4,5,6])
+# print(num)
+
+
+# friends = ["Aananda","Sandeep","Utsav","Ayush"]
+# choice = random.choice(friends)
+
+# print(choice + " will pay the bill.")
+
+fruits = ["Strawberries", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears"]
+vegetables = ["Spinach", "Kale", "Tomatoes", "Celery", "Potatoes"]
+ 
+dirty_dozen = [fruits, vegetables]
+ 
+print(dirty_dozen[1][1])
--- rockpaperscissors.py
+++ rockpaperscissors.py
@@ -1,3 +1,37 @@
 import random
-random_integer = random.randint(1,10)
-print(random_integer)
+
+user = input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors: ")
+
+if user == "0":
+    print("You choose rock")
+elif user =="1":
+    print("You choose paper")
+elif user=="2":
+    print("You choose Scissors")
+else:
+    print("Invalid input")
+
+item= ["Rock","Paper","Scissors"]
+cmp = random.choice(item)
+print(f"Computer choose {cmp}")
+
+if cmp=="Rock" and user=="0":
+    print("Draw!")
+elif cmp=="Rock" and user=="1":
+    print("You win")
+elif cmp == "Rock" and user == "2":
+    print("You lose!")
+
+elif cmp == "Paper" and user == "0":
+    print("You lose!")
+elif cmp == "Paper" and user == "1":
+    print("Draw!")
+elif cmp == "Paper" and user == "2":
+    print("You win!")
+
+elif cmp == "Scissors" and user == "0":
+    print("You win!")
+elif cmp == "Scissors" and user == "1":
+    print("You lose!")
+elif cmp == "Scissors" and user == "2":
+    print("Draw!")
